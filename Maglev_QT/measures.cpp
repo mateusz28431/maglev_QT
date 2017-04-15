3b0b0fa916295f976a0ab2a6ec9157c7cbf1e72a
Mateusz <mateusz28431@gmail.com>
2017-04-15 11:32:04 +0200

    Changed current calculation

--- Maglev_QT/measures.cpp
+++ Maglev_QT/measures.cpp
@@ -4,7 +4,7 @@ measures ADC2real( ADC_measures* adc )
 {
     measures m;
     m.input_voltage = (adc->Measure0 *3.3*1.532)/4095;
-    m.current_asc =  -5.0 +(adc->Measure10*3.3*1.468*5)/(4095*1.65);
+    m.current_asc =  -5.0 +(adc->Measure10*6.6*1.468)/(4095);
     m.current = 1.49 + (adc->Measure11*3.3*0.096)/4095;
     m.displacement_vol = 1.49 + (adc->Measure12*3.3*0.096)/4095;
 
